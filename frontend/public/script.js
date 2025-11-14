ade102e4bc9689b740e28dafbf740473bc0ba79b
kkattmos <khattiya.mos@gmail.com>
2025-11-14 17:10:40 +0700

    finished

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -3,6 +3,11 @@ const scoreDisplay = document.getElementById("scoreDisplay");
 const filterLeaderboardBtn = document.getElementById("filterLeaderboard");
 const showLeaderBoardBtn = document.getElementById("upDownArrow");
 const leaderBoardFrame = document.getElementById("leaderboardFrame");
+const headerLoginButton = document.getElementById("loginHeader");
+const formContainer = document.getElementById("formContainer");
+const screen = document.getElementById("screen");
+const loginError = document.getElementById("loginError");
+const createAccountError = document.getElementById("createAccountError");
 
 let score = 0;
 let filter = 'All';
@@ -30,24 +35,28 @@ const sawasdeeFile = [
 //CLICK NATTEE
 
 function handleClickNatteeDown(event) {
-    natteeBtn.setAttribute("src", "picture/speak.png");
+    
+    if (!event.repeat && formContainer.style.display === 'none') {
+        natteeBtn.setAttribute("src", "picture/speak.png");
 
-    const randomIndex = Math.floor(Math.random() * sawasdeeFile.length);
-    //console.log(randomIndex);
+        const randomIndex = Math.floor(Math.random() * sawasdeeFile.length);
+        //console.log(randomIndex);
 
-    const aud = new Audio(sawasdeeFile[randomIndex]);
-    aud.play();
-    score += 1;
-    scoreDisplay.innerText = score;
+        const aud = new Audio(sawasdeeFile[randomIndex]);
+        aud.play();
+        score += 1;
+        scoreDisplay.innerText = score;
+    }
+    
 };
 
 function handleClickNatteeUp(event) {
     natteeBtn.setAttribute("src", "picture/image.png");
 };
 
-natteeBtn.addEventListener('mousedown', () => handleClickNatteeDown(), false);
-document.addEventListener('keydown', () => handleClickNatteeDown(), false);
-natteeBtn.addEventListener('touchstart', () => handleClickNatteeDown(), false);
+natteeBtn.addEventListener('mousedown', (event) => handleClickNatteeDown(event), false);
+document.addEventListener('keydown', (event) => handleClickNatteeDown(event), false);
+natteeBtn.addEventListener('touchstart', (event) => handleClickNatteeDown(event), false);
 
 natteeBtn.addEventListener('mouseup', () => handleClickNatteeUp(), false);
 document.addEventListener('keyup', () => handleClickNatteeUp(), false);
@@ -95,3 +104,30 @@ function handleShowLeaderBoard() {
 }
 
 showLeaderBoardBtn.addEventListener("click", () => handleShowLeaderBoard());
+
+//LOGIN FORM
+function showForm2() {
+    formContainer.style.display = 'block';
+    screen.style.display = 'block';
+}
+
+headerLoginButton.addEventListener('click', () => showForm2());
+
+function showForm(formType) {
+    const loginForm = document.getElementById('login-form');
+    const createForm = document.getElementById('create-account-form');
+
+    if (formType === 'login') {
+        loginForm.style.display = 'block';
+        createForm.style.display = 'none';
+    } else if (formType === 'create') {
+        loginForm.style.display = 'none';
+        createForm.style.display = 'block';
+    }
+}
+
+function closeForm() {
+    formContainer.style.display = 'none';
+    screen.style.display = 'none';
+}
+
