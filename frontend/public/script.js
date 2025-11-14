63611248be75b2347e5cb9a65592f88efffa6eed
kkattmos <khattiya.mos@gmail.com>
2025-11-14 13:56:36 +0700

    improved ui

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,24 +1,41 @@
-const btn = document.getElementById("btn")
+const natteeBtn = document.getElementById("natteeBtn")
 const scoreDisplay = document.getElementById("scoreDisplay")
+const filterLeaderboardBtn = document.getElementById("filterLeaderboard")
 
 let score = 0;
+let filter = 'All';
 const sawasdeeFile = [
     'sawasdee/1.mp3',
     'sawasdee/4.mp3',
 ]
 
-btn.addEventListener('mousedown', async () => {
-    btn.setAttribute("src", "picture/speak.png");
+//CLICK NATTEE
+
+function handleClickNatteeDown(event) {
+    natteeBtn.setAttribute("src", "picture/speak.png");
 
     const randomIndex = Math.floor(Math.random() * sawasdeeFile.length);
-    console.log(randomIndex);
+    //console.log(randomIndex);
 
-    const nattee = new Audio(sawasdeeFile[randomIndex]);
-    nattee.play();
+    const aud = new Audio(sawasdeeFile[randomIndex]);
+    aud.play();
     score += 1;
     scoreDisplay.innerText = score;
-})
+};
 
-btn.addEventListener("mouseup", () => {
-    btn.setAttribute("src", "picture/image.png")
-})
+function handleClickNatteeUp(event) {
+    natteeBtn.setAttribute("src", "picture/image.png");
+};
+
+natteeBtn.addEventListener('mousedown', () => handleClickNatteeDown(), false);
+document.addEventListener('keydown', () => handleClickNatteeDown(), false);
+natteeBtn.addEventListener('touchstart', () => handleClickNatteeDown(), false);
+
+natteeBtn.addEventListener('mouseup', () => handleClickNatteeUp(), false);
+document.addEventListener('keyup', () => handleClickNatteeUp(), false);
+natteeBtn.addEventListener('touchend', () => handleClickNatteeUp(), false);
+
+//FILTER LEADERBOARD
+function handleChangeMode(event) {
+    if
+}
